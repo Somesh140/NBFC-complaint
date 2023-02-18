4748bbe77ce16ec0156b244288fda2253a74feed
Somesh140 <somesh140@gmail.com>
2023-02-19 00:04:20 +0530

    metadata dataingestion updated

---
+++ src/entity/metadata_entity.py
@@ -0,0 +1,38 @@
+from src.exception import CustomException
+import os, sys
+from src.logger import logger
+from src.utils import read_yaml_file, write_yaml_file
+from collections import namedtuple
+
+DataIngestionMetadataInfo = namedtuple("DataIngestionMetadataInfo", ["from_date", "to_date", "data_file_path"])
+
+class DataIngestionMetadata:
+
+    def __init__(self,metadata_file_path):
+        self.metadata_file_path = metadata_file_path
+
+    @property
+    def is_metadata_file_present(self):
+        return os.path.exists(self.metadata_file_path)
+
+    def write_meta_data_info(self,from_date:str, to_date:str, data_file_path:str):
+        try:
+            metadata_info=DataIngestionMetadataInfo(
+                from_date=from_date,
+                to_date=to_date,
+                data_file_path=data_file_path
+            )
+            write_yaml_file(file_path=self.metadata_file_path,data=metadata_info.yo_dict())
+        except Exception as e:
+            raise CustomException(e,sys)
+
+    def get_metadata_info(self) -> DataIngestionMetadataInfo:
+        try:
+            if not self.is_metadata_file_present:
+                raise Exception("No metadata file available")
+            metadata = read_yaml_file(self.metadata_file_path)
+            metadata_info = DataIngestionMetadataInfo(**(metadata))
+            logger.info(metadata)
+            return metadata_info
+        except Exception as e:
+            raise FinanceException(e, sys)
