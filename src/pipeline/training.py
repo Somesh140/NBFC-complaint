89caa5c44a6591fd6dcc6cec130ed57f16616f66
Somesh140 <somesh140@gmail.com>
2023-02-20 00:19:11 +0530

    pipeline updated with datavalidation

--- src/pipeline/training.py
+++ src/pipeline/training.py
@@ -1,6 +1,6 @@
-from src.entity.artifact_entity import (DataIngestionArtifact)
-from src.entity.config_entity import (DataIngestionConfig,TrainingPipelineConfig)
-from src.component import (DataIngestion)
+from src.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact)
+from src.entity.config_entity import (DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig)
+from src.component import (DataIngestion,DataValidation)
 from src.logger import logger
 from src.exception import CustomException
 import sys
@@ -20,10 +20,22 @@ class TrainingPipeline:
         except Exception as e:
             raise CustomException(e, sys)
 
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        try:
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=data_validation_config)
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+            return data_validation_artifact
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
     def start(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()   
-
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise CustomException(e, sys)
 
