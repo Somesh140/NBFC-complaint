ad1e02f2b45646b15ec42cc17b4d0d9434f82401
Somesh140 <somesh140@gmail.com>
2023-02-23 00:52:24 +0530

    estimator updated

--- src/ml/estimator.py
+++ src/ml/estimator.py
@@ -43,3 +43,35 @@ class ModelResolver:
             return latest_model_path
         except Exception as e:
             raise e
+
+class FinanceComplaintEstimator:
+
+    def __init__(self, model_dir=MODEL_SAVED_DIR,model_name=MODEL_NAME):
+        try:
+            self.model_resolver=ModelResolver(model_dir=model_dir,model_name=model_name)
+            self.model_dir = model_dir
+            self.loaded_model_path = None
+            self.__loaded_model = None
+        except Exception as e:
+            raise e
+
+    def get_model(self) -> PipelineModel:
+        try:
+            latest_model_path = self.model_resolver.get_best_model_path()
+            if latest_model_path != self.loaded_model_path:
+                self.__loaded_model = PipelineModel.load(latest_model_path)
+                self.loaded_model_path = latest_model_path
+            return self.__loaded_model
+        except Exception as e:
+            raise e
+
+    def transform(self, dataframe) -> DataFrame:
+        try:
+            model = self.get_model()
+            return model.transform(dataframe)
+        except Exception as e:
+            raise e
+
+
+
+    
